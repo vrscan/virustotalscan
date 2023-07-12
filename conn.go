0e29219dbbda54c931ee7df8672e5c7d1ddc9c51
vts <vstoscan@gmail.com>
2023-07-12 23:41:13 +0300

    fix not inited vrscan 2

--- conn.go
+++ conn.go
@@ -31,14 +31,14 @@ type Flusher struct {
 	c *ConnChecker
 }
 
-//continue work after flush
+// continue work after flush
 func (f *Flusher) Run() {
 	atomic.StoreInt32(&f.c.flushCalled, 0)
 }
 
-//flushes current checks to server and wait till em ends
-//stop launch for new
-//to continue checking call .Run()
+// flushes current checks to server and wait till em ends
+// stop launch for new
+// to continue checking call .Run()
 func (c *ConnChecker) Flush() *Flusher {
 	atomic.StoreInt32(&c.flushCalled, 1)
 	for {
@@ -57,7 +57,7 @@ func (c *ConnChecker) SetLogger(log *zipologger.Logger) {
 	c.log = log
 }
 
-//from conn to buffer
+// from conn to buffer
 func (c *ConnChecker) Read(b []byte) (int, error) {
 	n, e := c.conn.Read(b)
 	if c.logAll {
@@ -71,18 +71,20 @@ func (c *ConnChecker) Read(b []byte) (int, error) {
 		bc = append(bc, b[:n]...)
 
 		if atomic.LoadInt32(&c.useLocalVThelper) == 0 {
-			atomic.AddInt64(&c.runnedTasks, 1)
-			go func() {
-				defer atomic.AddInt64(&c.runnedTasks, -1)
-				found, _, err := c.vtscan.FastCheck(c.id, FC_CONN_READ, pnum, bc)
-				if found {
-					c.onalert()
-					return
-				}
-				if err != nil {
-					c.onerror(err)
-				}
-			}()
+			if c.vtscan != nil {
+				atomic.AddInt64(&c.runnedTasks, 1)
+				go func() {
+					defer atomic.AddInt64(&c.runnedTasks, -1)
+					found, _, err := c.vtscan.FastCheck(c.id, FC_CONN_READ, pnum, bc)
+					if found {
+						c.onalert()
+						return
+					}
+					if err != nil {
+						c.onerror(err)
+					}
+				}()
+			}
 		} else {
 			if a, _ := helperCheck(c.id, FC_CONN_READ, pnum, bc); a {
 				return 0, nil
@@ -94,7 +96,7 @@ func (c *ConnChecker) Read(b []byte) (int, error) {
 	return n, e
 }
 
-//to conn
+// to conn
 func (c *ConnChecker) Write(b []byte) (int, error) {
 	if c.logAll {
 		c.log.Printf("<= %s", string(b))
@@ -107,20 +109,22 @@ func (c *ConnChecker) Write(b []byte) (int, error) {
 		bc = append(bc, b...)
 
 		if atomic.LoadInt32(&c.useLocalVThelper) == 0 {
-			atomic.AddInt64(&c.runnedTasks, 1)
-			go func() {
-				defer atomic.AddInt64(&c.runnedTasks, -1)
-				found, desc, err := c.vtscan.FastCheck(c.id, FC_CONN_WRITE, pnum, bc)
-				if found {
-					if len(desc) > 1 {
-						c.conn.Write(desc)
+			if c.vtscan != nil {
+				atomic.AddInt64(&c.runnedTasks, 1)
+				go func() {
+					defer atomic.AddInt64(&c.runnedTasks, -1)
+					found, desc, err := c.vtscan.FastCheck(c.id, FC_CONN_WRITE, pnum, bc)
+					if found {
+						if len(desc) > 1 {
+							c.conn.Write(desc)
+						}
+						return
 					}
-					return
-				}
-				if err != nil {
-					c.onerror(err)
-				}
-			}()
+					if err != nil {
+						c.onerror(err)
+					}
+				}()
+			}
 		} else {
 			if a, b := helperCheck(c.id, FC_CONN_WRITE, pnum, bc); a {
 				if len(b) > 0 {
@@ -159,13 +163,9 @@ func (c *ConnChecker) SetWriteDeadline(t time.Time) error {
 }
 
 /*
-	Creates MITM conn, called deffered alert if something found
+Creates MITM conn, called deffered alert if something found
 */
 func NewDefferedConnChecker(useLocalVThelper bool, conn net.Conn, vtscan *Vtscan, onalert func(), onerror func(err error)) *ConnChecker {
-	if vtscan == nil {
-		return nil
-	}
-
 	id := uuid.New()
 
 	useLocalVThelper_i := int32(0)
@@ -183,7 +183,7 @@ func NewDefferedConnChecker(useLocalVThelper bool, conn net.Conn, vtscan *Vtscan
 	}
 }
 
-//net.Conn if needed
+// net.Conn if needed
 func (c *ConnChecker) Conn() net.Conn {
 	return c
 }
